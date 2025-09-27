e8228897aa439581c88ebcdd9248148e3d1315cb
Nathan Wright <natiouswright@gmail.com>
2025-09-27 16:05:30 -0500

    Exercise 01 complete

---
+++ answers/01-contains-duplicates/Duplicates.java
@@ -0,0 +1,27 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Duplicates {
+
+	public static void main(final String[] args) {
+        final int[] nums = {1,3,6,1,2};
+
+        System.out.println("Dupilicates found: " + containsDuplicates(nums));
+
+        return;
+    }
+
+    public static boolean containsDuplicates(int[] nums) {
+        Set<Integer> checkedNumbers = new HashSet<Integer>();
+
+        // checks all numbers in the input array
+        for (int num : nums) {
+            if (checkedNumbers.contains(num)) {
+                return true;
+            }
+        }
+
+        // only reach this when no duplicates have been found
+        return false;
+    }
+}
