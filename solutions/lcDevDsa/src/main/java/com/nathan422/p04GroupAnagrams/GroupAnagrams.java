74da97e639a5f36d8f30534d3cf01630750b74ee
Nathan Wright <natiouswright@gmail.com>
2025-10-04 12:31:56 -0500

    Added method for comparing anagrams

--- solutions/lcDevDsa/src/main/java/com/nathan422/p04GroupAnagrams/GroupAnagrams.java
+++ solutions/lcDevDsa/src/main/java/com/nathan422/p04GroupAnagrams/GroupAnagrams.java
@@ -6,4 +6,27 @@ public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         return new ArrayList<List<String>>();
     }
+
+    private boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int[] difference = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            difference[a.charAt(i) - 'a']++;
+            difference[b.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < difference.length; i++) {
+            if (difference[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
 }
